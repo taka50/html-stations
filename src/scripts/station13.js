6976246cde0b8781d37fac4db71fc4d6ae40d233
takaN20 <ceta20024@g.nihon-u.ac.jp>
2024-01-12 16:37:31 +0900

    station13から15までを終了

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -2,10 +2,18 @@ data = "global data";
 
 // このreturnDataに代入される関数の形をアロー関数に変えて"global data"が表示されるようにしてください
 // ↓関数の中の処理は変更しないでください
-const returnThisData = function () {
+
+/* アロー関数 this.data は宣言された時点で、thisを確定、
+   すなわち、どこで呼び出しても宣言時点のdata(ここでは１行目)を呼び出す */
+const returnThisData = (data) => {
   return this.data;
 };
 // ↑関数の中の処理は変更しないでください
+
+/* 通常の関数 this.data は呼び出し元のオブジェクトのdata 呼び出す場所で中身が変わる */
+// const returnThisData = function () {
+//   return this.data;
+// };
 
 const object = {
   data: "object data",
